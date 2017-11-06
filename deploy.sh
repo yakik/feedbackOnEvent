30d30527eeb0d2276d17dcf64ac578e0cceddfd3
Yaki <yaki.koren@gmail.com>
2017-11-07 00:20:42 +0200

    changed startup, removed sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-# Script to deploy the app
-# This file is needed only when you want to deploy the app to a Linux VM
-sudo apt-get -y install npm
-sudo apt-get -y install nodejs
-npm init
-npm install express -y
-nodejs server.js
