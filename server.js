1d6c654b1e1cbfc98ad434084a75c9399ad418bb
Yaki <yaki.koren@gmail.com>
2017-11-08 19:28:41 +0200

    express

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
-var http = require('http');
 
-var server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "html"});
-    response.write('<!DOCTYPE html><html><head><title>Agile Sparks Event Feedback</title></head><body></body></html>')
-    response.end("Hello W orld!");
-});
+var express = require('express');
+var app = express();
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+app.get('/', function (req, res) {
+   res.send('<!DOCTYPE html><html><head><title>Agile Sparks Event Feedback</title></head><body></body></html>');
+})
 
-console.log("Server running at http://localhost:%d", port);
+// This responds a POST request for the homepage
+//app.post('/', function (req, res) {
+//    console.log("Got a POST request for the homepage");
+//    res.send('Hello POST');
+//})
+
+// This responds a GET request for the /list_user page.
+//app.get('/list_user', function (req, res) {
+//    console.log("Got a GET request for /list_user");
+//    res.send('Page Listing');
+//})
+
+
+var server = app.listen(1337, function () {});
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port);
+
+
+
+
